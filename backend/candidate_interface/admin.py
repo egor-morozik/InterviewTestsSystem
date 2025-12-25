fd73ff5a514ec1692e162c60888afefd787d7806
egor-morozik <egor.mrz04@gmail.com>
2025-12-25 20:18:18 +0300

    Add models to admin panel

--- backend/candidate_interface/admin.py
+++ backend/candidate_interface/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
+from .models import Candidate, Invitation, Answer
+from interviewer_interface.models import TestTemplate
 
-# Register your models here.
+
+@admin.register(Candidate)
+class CandidateAdmin(admin.ModelAdmin):
+    list_display  = ('email', 'full_name')
+    search_fields = ('email', 'full_name')
+
+    def invitation_count(self, obj):
+        return obj.invitations.count()
+    invitation_count.short_description = "Приглашений"
+
+@admin.register(Invitation)
+class InvitationAdmin(admin.ModelAdmin):
+    list_display = ('candidate', 'test_template', 'unique_link_short')
+    list_filter = ('test_template', 'sent', 'created_at')
+    search_fields = ('candidate__email', 'test_template__name')
+    actions = ['send_selected_invitations']
+    readonly_fields = ('unique_link',)
+    def unique_link_short(self, obj):
+        return str(obj.unique_link)[:8] + "..."
+    unique_link_short.short_description = "Ссылка"
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('invitation', 'question_truncated', 'score', 'evaluated_at')
+    list_filter = ('invitation__test_template', 'score', 'invitation__candidate')
+    search_fields = ('invitation__candidate__email', 'question__text')
+    readonly_fields = ('invitation', 'question', 'response', 'score')
+
+    def question_truncated(self, obj):
+        return obj.question.text[:60] + "..." if len(obj.question.text) > 60 else obj.question.text
+    question_truncated.short_description = "Вопрос"
--- backend/interviewer_interface/admin.py
+++ backend/interviewer_interface/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TestTemplate, Question
+
+
+@admin.register(TestTemplate)
+class TestTemplateAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name', 'description')
+
+    def question_count(self, obj):
+        return obj.questions.count()
+    question_count.short_description = "Вопросов"
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('text_truncated', 'template', 'answer_truncated')
+    list_filter = ('template',)
+    search_fields = ('text',)
+
+    def text_truncated(self, obj):
+        return obj.text[:80] + "..." if len(obj.text) > 80 else obj.text
+    def answer_truncated(self, obj):
+        return obj.correct_answer[:80] + "..." if len(obj.correct_answer) > 80 else obj.correct_answer
+    text_truncated.short_description = "Вопрос"
+    answer_truncated.short_description = "Правильный ответ"
