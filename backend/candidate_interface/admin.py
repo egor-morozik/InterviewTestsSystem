a0064a926529bf53004ddca7c1926e2a0eae2fed
egor-morozik <egor.mrz04@gmail.com>
2025-12-25 21:55:34 +0300

    Add prototype of generating and sending urls for candidates

--- backend/candidate_interface/admin.py
+++ backend/candidate_interface/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 from django.urls import reverse
+from django.http import HttpResponseRedirect
 from .models import Candidate, Invitation, Answer
 from interviewer_interface.models import TestTemplate
 
@@ -16,14 +17,35 @@ class CandidateAdmin(admin.ModelAdmin):
 
 @admin.register(Invitation)
 class InvitationAdmin(admin.ModelAdmin):
-    list_display = ('candidate', 'test_template', 'unique_link_short')
+    list_display = ('candidate', 'test_template', 'unique_link_short', 'sent', 'created_at', 'send_link')
     list_filter = ('test_template', 'sent', 'created_at')
     search_fields = ('candidate__email', 'test_template__name')
     actions = ['send_selected_invitations']
-    readonly_fields = ('unique_link',)
+    readonly_fields = ('unique_link', 'created_at')
+
     def unique_link_short(self, obj):
         return str(obj.unique_link)[:8] + "..."
     unique_link_short.short_description = "Ссылка"
+
+    def send_link(self, obj):
+        url = reverse('admin:send_invitation', args=[obj.pk])
+        return format_html('<a href="{}">Отправить</a>', url)
+    send_link.short_description = "Действие"
+
+    def send_selected_invitations(self, request, queryset):
+        self.message_user(request, f"Отправлено {queryset.count()} приглашений.")
+    send_selected_invitations.short_description = "Отправить выбранные приглашения"
+
+    def get_urls(self):
+        from django.urls import path
+        urls = super().get_urls()
+        custom_urls = [
+            path('<int:invitation_id>/send/', self.admin_site.admin_view(self.send_single_invitation), name='send_invitation'),
+        ]
+        return custom_urls + urls
+
+    def send_single_invitation(self, request, invitation_id):
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', 'admin:candidate_interface_invitation_changelist'))
 
 @admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
