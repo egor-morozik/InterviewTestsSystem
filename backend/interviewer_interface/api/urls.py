404577a2287e370dca4ac21947607a0a16884f13
egor-morozik <egor.mrz04@gmail.com>
2026-01-06 11:58:26 +0300

    Delete isort job

--- backend/interviewer_interface/api/urls.py
+++ backend/interviewer_interface/api/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from .views import TestTemplateDetailView, TestTemplateListView
 
 urlpatterns = [
-    path("templates/", TestTemplateListView.as_view(), name="template_list"),
+    path(
+        "templates/",
+        TestTemplateListView.as_view(),
+        name="template_list",
+    ),
     path(
         "templates/<int:pk>/", TestTemplateDetailView.as_view(), name="template_detail"
     ),
