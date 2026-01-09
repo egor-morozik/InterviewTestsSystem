83e4f0d40857438defe19d7c9d932f6472c30cdf
egor-morozik <egor.mrz04@gmail.com>
2026-01-09 10:06:01 +0300

    Delete tamplates

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -61,22 +61,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "config.urls"
 
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [
-            BASE_DIR / "templates",
-        ],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
-]
+TEMPLATES = []
 
 ASGI_APPLICATION = "config.asgi.application"
 
